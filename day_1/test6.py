6063e08db2e0870bfe8f10008c4df9405bfe0b0a
dony wijaya <donywijaya@detik.com>
2020-01-31 15:45:50 +0700

    push 4

---
+++ day_1/test6.py
@@ -0,0 +1,10 @@
+number = 5
+
+def summation(first, second):
+    total = first + second + number
+    return total
+
+summation(10, 20)
+
+print("The first number we initialised was " + str(number))
+print("The total after summation is " + str(total))
